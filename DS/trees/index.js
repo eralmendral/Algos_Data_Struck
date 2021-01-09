347338b9fbc2d3c4e2fcdc89bb633feccf44b72f
eralmendral <eralmendral@gmail.com>
2021-01-09 22:53:39 +0800

    ds: trees, add and remove node children

---
+++ DS/trees/index.js
@@ -0,0 +1,19 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children = this.children.filter(child => child.data !== data);
+  }
+}
+
+class Tree {
+}
+
+module.exports = { Tree, Node };
