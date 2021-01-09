490e602564e9773ead45e6a5121106befe3a9a61
eralmendral <eralmendral@gmail.com>
2021-01-09 21:59:57 +0800

    ds: get middle node of a linkedlist

---
+++ DS/linkedlists/midpoint/index.js
@@ -0,0 +1,14 @@
+
+function midpoint(list){
+  let slow = list.head;
+  let fast = list.head;
+
+  while(fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow; // the midpoint
+}
+
+module.exports = midpoint;
